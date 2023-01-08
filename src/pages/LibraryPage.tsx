e0ffee8ef65725e0383988b6b97bfc4257428f13
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-08 21:46:18 +0200

    fixed libraryPage

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -1,36 +1,24 @@
 import { Outlet, useSearchParams } from 'react-router-dom';
-import { fetchSearchMovies } from '../api/themoviedbAPI';
-import { useEffect, useState, Suspense } from 'react';
-import { toast } from 'react-toastify';
+import { useState, Suspense } from 'react';
 import { VideoGallery } from 'components/VideoGallery';
 import { Loader } from 'components/Loader';
 import { IMovies } from 'types/movies';
 
 export default function LiabraryPage() {
-  const [movies, setMovies] = useState<IMovies[]>([]);
+  const [movies, setMovies] = useState<IMovies[]>(() => {
+    const value = localStorage.getItem('watchedMovies');
+    if (typeof value === 'string') {
+      return JSON.parse(value);
+    }
+  });
+  console.log(movies);
+
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query') ?? '';
 
-  useEffect(() => {
-    if (query === '') return;
-    async function fetchMovie() {
-      try {
-        const data = await fetchSearchMovies(query);
-        setMovies(data);
-        if (!data.length) {
-          toast.info(`${query} not found`);
-          return;
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchMovie();
-  }, [query]);
-
   return (
     <main>
-      {movies && <VideoGallery movies={movies} />}
+      {movies ? <VideoGallery movies={movies} /> : <p>no movies</p>}
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
