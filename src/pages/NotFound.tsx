25ec71a449bad682d414bddb6aacf457f0ea0d60
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-09 13:15:04 +0200

    fixed text

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <p className="text-center p-6 text-2xl">
       This page was not found, please return to
-      <Link className="text-blue" to="/">
+      <Link className="text-blue text-semibold" to="/">
         Home page
       </Link>
     </p>
