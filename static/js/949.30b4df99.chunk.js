02c4e48fabdf557cd6e6daf965c8f43dc679cf3c
ruslanbabkin01 <103895115+ruslanbabkin01@users.noreply.github.com>
2023-01-11 09:37:17 +0000

    Deploying to gh-pages from @ ruslanbabkin01/movies-library@31938e4521cbc976b491a5193080d83ba24a6d1a 🚀

---
+++ static/js/949.30b4df99.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmovies_library=self.webpackChunkmovies_library||[]).push([[949],{4949:function(e,t,s){s.r(t),s.d(t,{default:function(){return a}});var r=s(6731),n=s(184);function a(){return(0,n.jsxs)("p",{className:"text-center p-6 text-2xl",children:["This page was not found, please return to",(0,n.jsx)(r.rU,{className:"text-blue text-semibold",to:"/",children:"Home page"})]})}}}]);
+//# sourceMappingURL=949.30b4df99.chunk.js.map
